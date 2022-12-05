1d231c3f232830b8d9c2058c627b03077365a05f
Kirill Andreev <kirill.andreev.239@gmail.com>
2022-12-05 12:46:45 +0100

    Day 5

---
+++ src/Day05.kt
@@ -0,0 +1,53 @@
+private fun input(): Stacks {
+    val lines = readInput("Day05")
+    val stacksInput = lines.takeWhile { it.isNotEmpty() }
+    val stacksCount = (stacksInput.last().length + 2) / 4
+    val stacks = ArrayList<ArrayDeque<Char>>()
+    repeat(stacksCount) { stacks.add(ArrayDeque()) }
+    stacksInput.forEach {
+        for (i in 0 until stacksCount) {
+            val charInd = 4 * i + 2
+            if (charInd >= it.length) continue
+            val curChar = it[4 * i + 1]
+            if (curChar.isUpperCase()) {
+                stacks[i].addFirst(curChar)
+            }
+        }
+    }
+    val ops = lines.subList(stacksInput.size + 1, lines.size).map {
+        val split = it.split(" ")
+        OP(split[1].toInt(), split[3].toInt() - 1, split[5].toInt() - 1)
+    }
+    return Stacks(stacks, ops)
+}
+
+private fun part1(input: Stacks): String {
+    val stacks = input.stacks
+    input.ops.forEach { op ->
+        repeat(op.count) {
+            stacks[op.to].add(stacks[op.from].removeLast())
+        }
+    }
+    return stacks.map { it.last() }.joinToString(separator = "")
+}
+
+private fun part2(input: Stacks): String {
+    val stacks = input.stacks
+    input.ops.forEach { op ->
+        val tmp = mutableListOf<Char>()
+        repeat(op.count) {
+            tmp.add(stacks[op.from].removeLast())
+        }
+        tmp.reversed().forEach { stacks[op.to].add(it) }
+    }
+    return stacks.map { it.last() }.joinToString(separator = "")
+}
+
+fun main() {
+    println(part1(input()))
+    println(part2(input()))
+}
+
+data class Stacks(val stacks: List<ArrayDeque<Char>>, val ops: List<OP>)
+
+data class OP(val count: Int, val from: Int, val to: Int)
---
+++ src/DayXX.kt
@@ -0,0 +1,16 @@
+private fun input(): List<String> {
+    return readInput("DayXX")
+}
+
+private fun part1(input: List<String>): Int {
+    return input.size
+}
+
+private fun part2(input: List<String>): Int {
+    return input.size
+}
+
+fun main() {
+    println(part1(input()))
+    println(part2(input()))
+}
