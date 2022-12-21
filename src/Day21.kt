81663874fa7b4dae9567a35380b42915c6e10085
Kirill Andreev <kirill.andreev.239@gmail.com>
2022-12-21 20:50:00 +0100

    refactor

--- src/Day21.kt
+++ src/Day21.kt
@@ -7,7 +7,7 @@ private fun input(): Map<String, M> {
         } else {
             parent[expS[2]] = id
             parent[expS[0]] = id
-            id to M(id, f = expS[0], s = expS[2], op = expS[1])
+            id to M(id, l = expS[0], r = expS[2], op = expS[1])
         }
     }
 }
@@ -20,7 +20,7 @@ private fun part1(): Long {
 
 private fun calc(id: String): Long {
     val m = monkeys[id]!!
-    return m.const ?: m.calc(calc(m.f!!), calc(m.s!!))
+    return m.const ?: m.calc(calc(m.l!!), calc(m.r!!))
 }
 
 private fun part2(): Long {
@@ -30,8 +30,8 @@ private fun part2(): Long {
 private fun calculateHumn(id: String): Long {
     val p = parent[id]!!
     val pM = monkeys[p]!!
-    val left = pM.f!! == id
-    val other = if (left) calc(pM.s!!) else calc(pM.f!!)
+    val left = pM.l!! == id
+    val other = if (left) calc(pM.r!!) else calc(pM.l!!)
     return if (p == "root") other else pM.calcInv(calculateHumn(p), other, left)
 }
 
@@ -43,8 +43,8 @@ fun main() {
 private data class M(
     val id: String,
     val const: Long? = null,
-    val f: String? = null,
-    val s: String? = null,
+    val l: String? = null,
+    val r: String? = null,
     val op: String? = null
 ) {
     fun calc(a: Long, b: Long): Long {
