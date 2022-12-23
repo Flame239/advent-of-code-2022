9d5f350ba9da4fd9203883dddac6b67e0259dbca
Kirill Andreev <kirill.andreev.239@gmail.com>
2022-12-23 11:50:39 +0100

    Day 23

---
+++ src/Day23.kt
@@ -0,0 +1,81 @@
+private fun input(): Array<BooleanArray> {
+    val gs = readInput("Day23")
+    val padding = 100
+    val h = gs.size + 2 * padding
+    val w = gs[0].length + 2 * padding
+    val arr = Array(h) { BooleanArray(w) }
+    gs.forEachIndexed { rowInd, row ->
+        row.forEachIndexed { i, c ->
+            arr[rowInd + padding][i + padding] = (c == '#')
+        }
+    }
+    return arr
+}
+
+fun moveElves(g: Array<BooleanArray>, part1: Boolean): Int {
+    val elves = HashSet<CC>()
+    for (i in g.indices) for (j in g[0].indices) if (g[i][j]) elves.add(CC(i, j))
+    var d = 0
+    repeat(if (part1) 10 else Int.MAX_VALUE) {
+        val moves = HashMap<CC, MutableList<Move>>()
+        elves.forEach { e ->
+            val hasN = g[e.i - 1][e.j] || g[e.i - 1][e.j + 1] || g[e.i - 1][e.j - 1]
+            val hasS = g[e.i + 1][e.j] || g[e.i + 1][e.j + 1] || g[e.i + 1][e.j - 1]
+            val hasW = g[e.i][e.j - 1] || g[e.i + 1][e.j - 1] || g[e.i - 1][e.j - 1]
+            val hasE = g[e.i][e.j + 1] || g[e.i + 1][e.j + 1] || g[e.i - 1][e.j + 1]
+            if (!hasN && !hasW && !hasS && !hasE) return@forEach
+            val has = arrayOf(hasN, hasS, hasW, hasE)
+            for (di in 0..3) {
+                val elfD = (d + di) % 4
+                if (!has[elfD]) {
+                    val from = CC(e.i, e.j)
+                    val to = e.move(elfD)
+                    moves.getOrPut(to) { mutableListOf() }.add(Move(from, to))
+                    break
+                }
+            }
+        }
+        val possibleMoves = moves.filter { (_, moves) -> moves.size == 1 }
+        if (possibleMoves.isEmpty() && !part1) return it + 1
+        possibleMoves.forEach { (_, moves) ->
+            val m = moves[0]
+            g[m.from.i][m.from.j] = false
+            g[m.to.i][m.to.j] = true
+
+            elves.remove(m.from)
+            elves.add(m.to)
+        }
+        d++
+    }
+    return if (part1) findArea(elves, g) else -1
+}
+
+private fun findArea(elves: HashSet<CC>, g: Array<BooleanArray>): Int {
+    var count = 0
+    for (i in elves.minOf { it.i }..elves.maxOf { it.i }) {
+        for (j in elves.minOf { it.j }..elves.maxOf { it.j }) {
+            if (!g[i][j]) count++
+        }
+    }
+    return count
+}
+
+
+fun main() {
+    measure { moveElves(input(), true) }
+    measure { moveElves(input(), false) }
+}
+
+private data class CC(val i: Int, val j: Int) {
+    fun move(d: Int): CC {
+        return when (d) {
+            0 -> CC(i - 1, j)
+            1 -> CC(i + 1, j)
+            2 -> CC(i, j - 1)
+            3 -> CC(i, j + 1)
+            else -> error("D")
+        }
+    }
+}
+
+private data class Move(val from: CC, val to: CC)
